834dcbc56f79d2cd798f85cce3995f67ce79bc76
arch <serf.jp@gmail.com>
2022-05-14 02:10:27 +0200

    more comments, YaY

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
+-- more imports!! YAY!!
 import Graphics.Image as I
 import Graphics.Image.IO
 
 import System.IO
 
+-- not forget to import our friend, module Check from Project /src/
 import Check
 
 ------------------
@@ -13,14 +15,20 @@ import Check
 
 main :: IO()
 main = do
+    -- Image generation through pixelCheck from module Check by checking each pixel of the given matrix (hoehe and breite are set in Check)
     let manImage = makeImageR VS (hoehe, breite) (\(j, i) -> PixelY (Check.pixelCheck i j))
-   
+    
+    -- debug Information
     putStrLn "Started"
     
+    -- write generated Image to file to Project /
     writeImage "mandelbrot_set.png" (manImage::Image VS Y Double)     
     
+    -- more Debugging information. everyone loves debugging information
     putStrLn "showing image"
 
+    -- display the image via feh Image viewer - idk why there is a bool. check docks on Graphics.Image.IO
     displayImageUsing fehViewer True manImage
     
+    -- thats just dumb at this point
     putStrLn "done"
--- src/Check.hs
+++ src/Check.hs
@@ -21,22 +21,26 @@ manFn x c = x*x + c
 -- new New implementation --
 ----------------------------
 
+-- checks if the recursion exit condition is met
 checker :: Complex Double -> Bool
 checker comp = (C.realPart comp) * (C.realPart comp) + (C.imagPart comp) * (C.imagPart comp) > 42
 
-
+-- well, function that returns Gradient - number from [0,1] based of where it was on a scale from 1 to 255
 manGradient n = (nD/255)
     where
         nD= fromIntegral n
 
+-- just setting n to 0 xD
 compNumbGradient = compNumbGradientSub 0 
 
+-- recursibve function that returns the iteraation when it exits
 compNumbGradientSub :: Int -> Complex Double -> Complex Double -> Int -> Double
 compNumbGradientSub n x c haltN
   | n == haltN = manGradient n
   | checker x = manGradient n
   | otherwise = compNumbGradientSub (n+1) (manFn x c) c haltN
 
+-- "adapter" function that adapts input of the pixelCheck complexPointCheck
 complexPointCheck :: Double -> Double -> Double
 complexPointCheck a b = compNumbGradient (0:+0) z 255 
     where
